42860f8a37c33305b854633f80be1b34c9e6caa0
fjarlaegur <127120402+fjarlaegur@users.noreply.github.com>
2024-04-27 22:32:42 +0100

    Update my_dashboard.py

--- my_dashboard.py
+++ my_dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 from sodapy import Socrata
 
 
@@ -22,7 +23,7 @@ def load_data(nrows: int) -> object:
 
     data.drop(columns="location", inplace=True)
 
-    # dropping rows with emply lat long columns
+    # dropping rows with emply lat, long, and street name columns
 
     data.dropna(subset=["latitude", "longitude", "on_street_name"], inplace=True)
 
@@ -36,24 +37,97 @@ def load_data(nrows: int) -> object:
     return data
 
 
+@st.cache_data(persist=True)
+def convert_to_csv(dataframe: object) -> object:
+    """Converting the dataframe to .csv to download it later"""
+    return dataframe.to_csv().encode("utf-8")
+
+
+@st.cache_data(persist=True)
+def top_five_cases(option):
+    if option == "Deadliest crashes":
+        data["total_severity"] = pd.to_numeric(
+            data["number_of_persons_injured"] +
+            data["number_of_persons_killed"]
+        )
+        newdata = data.sort_values("total_severity", ascending=False).head(5)
+        return newdata
+    else:
+        pass
+
+
 data = load_data(100_000)
+data_for_download = data
 
 st.title("New York City Motor Vehicle Collisions :collision:")
 
 # density heatmap for crash hotspots
+
+st.subheader("Crash Frequency Based on Location")
+st.markdown("""This is the :rainbow[heatmap] of New York City, showing amount of motor
+            accident activity by street.""")
+
 # calculating crash counts
 
 crash_counts = data.groupby("on_street_name").size().reset_index(name="crash_counts")
 data = data.merge(crash_counts, on="on_street_name")
 
-fig = px.density_mapbox(data, lat='latitude', lon='longitude', z="crash_counts", radius=10,
-                        center=dict(lat=data['latitude'].mean(), lon=data['longitude'].mean()),
-                        zoom=10, mapbox_style="carto-darkmatter", opacity=0.5,
+# drawing a heatmap
+
+fig = px.density_mapbox(data, lat='latitude', lon='longitude',
+                        z="crash_counts", radius=3,
+                        center=dict(lat=40.67812, lon=-73.92165),
+                        opacity=0.7,
+                        zoom=9, mapbox_style="carto-positron",
+                        color_continuous_scale="rainbow",
+                        range_color=(0, data["crash_counts"].max()),
                         hover_name="on_street_name", hover_data="crash_counts",
-                        title='Crash Hotspots Density Heatmap')
+                        width=650, height=600)
+
+fig.add_trace(
+    go.Scattermapbox(
+        lat=data["latitude"],
+        lon=data["longitude"],
+        mode="markers",
+        showlegend=False,
+        hoverinfo="skip",
+        marker={
+            "color": data["crash_counts"],
+            "size": data["crash_counts"].fillna(0),
+            "coloraxis": "coloraxis",
+            "sizeref": (data["crash_counts"].max()) / 15 ** 2,
+            "sizemode": "area",
+        },
+    )
+)
 st.write(fig)
 
+
+#  creating top-5 cases
+st.subheader("Top-5 of the most extreme cases. I want to see:")
+option = st.selectbox(
+    "Top-5",
+    index=None,
+    options=[
+        "Deadliest crashes",
+        "Contibuting factors",
+        "Most dangerous streets"
+    ],
+    label_visibility="collapsed"
+)
+
+st.write(top_five_cases(option))
 
 if st.checkbox("Show Raw Data", False):
     st.subheader("Raw Data")
     st.write(data)
+
+    csv = convert_to_csv(data_for_download)
+    st.download_button(
+        "Download data as .csv",
+        data=csv,
+        file_name="NYC_Motor_Vehicle_Collisions_2023.csv",
+        mime="text/csv",
+    )
+    st.caption("""Warning! This file is quite large and
+               may cause slowness/freezes of your editor.""")
